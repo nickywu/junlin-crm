3aedb83b600ed014221300efb1b1b4495b0fd171
fantasy <atsep@qq.com>
2025-12-10 23:56:43 +0800

    修复菜单列表图标和操作按钮不显示

--- web/src/views/system/menu/index.vue
+++ web/src/views/system/menu/index.vue
@@ -40,6 +40,16 @@
         <s-button icon="down-square-outlined" @click="expandAll">展开</s-button>
         <s-button icon="up-square-outlined" @click="collapseAll">折叠</s-button>
       </template>
+
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.dataIndex === 'action'">
+          <a @click="openModal(true, { pid: record.id })">添加</a>
+          <a-divider type="vertical" />
+          <a @click="openModal(true, record)">修改</a>
+          <a-divider type="vertical" />
+          <s-confirm-button label="删除" title="确认要删除吗?" @confirm="handleDelete(record.id)" />
+        </template>
+      </template>
     </s-table>
     <menu-form @register="registerModal" @save-success="search" :treeData="treeData" />
   </page-view>
@@ -52,7 +62,7 @@ import { getMenuList, destroy } from "@/api/system/menu";
 import menuForm from "./from.vue";
 import { treeEach } from "@/utils";
 import { useTreeSearch } from "./useTreeSearch";
-
+import type { TableColumnProps } from "@/components/Table";
 export interface TreeData {
   id: string | number;
   value: number;
@@ -63,18 +73,11 @@ export interface TreeData {
 
 type Key = string | number;
 
-const {
-  searchText,
-  expandedRowKeys,
-  highlightText,
-  doSearch,
-  resetSearch
-} = useTreeSearch('title');
+const { searchText, expandedRowKeys, highlightText, doSearch, resetSearch } = useTreeSearch("title");
 const treeData = ref<TreeData[]>([]);
 const searchInput = ref();
 
-
-const columns = [
+const columns: TableColumnProps[] = [
   {
     title: "菜单名称",
     dataIndex: "title",
@@ -94,7 +97,7 @@ const columns = [
   {
     title: "图标",
     dataIndex: "icon",
-    customRender: ({ text }: { text: string }) => h("s-icon", { type: text })
+    customRender: ({ text }) => <s-icon type={text} />
   },
   {
     title: "路由地址",
@@ -160,12 +163,10 @@ const collapseAll = () => {
   expandedRowKeys.value = [];
 };
 
-
-
 // 搜索处理
-const handleSearch = (selectedKeys: string[], confirm: () => void, dataIndex: string) => {
+const handleSearch = (selectedKeys: string[], confirm: () => void) => {
   confirm();
-  doSearch(selectedKeys[0] || '', treeData);
+  doSearch(selectedKeys[0] || "", treeData);
 };
 
 // 重置处理
@@ -174,7 +175,6 @@ const handleReset = (clearFilters: () => void, confirm: () => void) => {
   resetSearch();
   confirm();
 };
-
 
 const [registerModal, { openModal }] = useModal();
 </script>
